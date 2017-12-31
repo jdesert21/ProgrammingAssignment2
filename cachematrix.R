d6cee3509c4316d5f633a6c6f07f3e79b3f47008
jdesert21 <jd.schiano@gmail.com>
2017-12-31 23:02:26 +0100

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+##I sweated a lot to test those 2 functions... 
+## The first one create an object to cache a matrix, its inverse with getters
+## and setters. The second one is here to test whether or not the inverse have
+## already been computed. 
 
-## Write a short comment describing this function
-
+#The lines I tested with are written at the end of the program
 makeCacheMatrix <- function(x = matrix()) {
-
+      inverse <- NULL
+      set <- function(y) {
+            x <<- y
+            inverse <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(inv) inverse <<- inv
+      getinverse <- function() inverse
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
+}
+##If the inverse have been computed (test with the getter),
+## the inverse is returned. 
+## If not, the inverse is calculated.
+cachSolve <- function(x, ...) {
+      inverse <- x$getinverse()
+      if(!is.null(inverse)) {
+            message("getting cached data")
+            return(inverse)
+      }
+      data <- x$get()
+      inverse <- solve(a=data, ...)
+      x$setinverse(inverse)
+      inverse
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+##Tested with the following lines : 
+##mat1=matrix(c(1,2,3,4),nrow=2,ncol=2)
+##x<-makeCacheMatrix(mat1)
+##z<-cachSolve(x)
